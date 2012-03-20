71964ebe3070fc014efdcd2fd58c372dbe05e9ed
unknown <ali@ali-PC.(none)>
2012-03-20 23:23:01 +0100

    proper handling of the files (why u no support Python 2.7)

--- page-generator/pageGenerator.py
+++ page-generator/pageGenerator.py
@@ -16,7 +16,26 @@ def getPageHref(dataFileName):
 
 
 def getTitle(lines):
-    return lines[0]
+    firstLine = lines[0]
+    if not firstLine[-1].isalpha():
+        firstLine = firstLine[:-1]
+    return firstLine
+
+
+def clearDestinationFolder(destinationFolderName):
+    if os.path.exists(destinationFolderName):
+        shutil.rmtree(destinationFolderName)
+        os.makedirs(destinationFolderName)
+
+
+def readPageTemplate():
+    pageTemplateFile = open('templates/pageTemplate.html')
+    try:
+        pageTemplateFile = open('templates/pageTemplate.html')
+        template = pageTemplateFile.read()
+        return template
+    finally:
+        pageTemplateFile.close()
 
 
 def main():
@@ -24,14 +43,13 @@ def main():
     dataFileExtension = '.txt'
     destinationFolderName = '../generated-pages/pages/'
 
-    if os.path.exists(destinationFolderName):
-        shutil.rmtree(destinationFolderName)
-        os.makedirs(destinationFolderName)
+    clearDestinationFolder(destinationFolderName)
 
-    pageTemplateFile = open('templates/pageTemplate.html')
-    template = pageTemplateFile.read()
+    template = readPageTemplate()
 
     dataFileNameList = getDataFileNameList(dataFolder, dataFileExtension)
+
+    indexContent = ''
 
     for i in range(0, len(dataFileNameList)):
         dataFileName = dataFileNameList[i]
@@ -42,26 +60,42 @@ def main():
             nextPageHref = getPageHref(dataFileNameList[i+1])
 
         outputFileName = getPageHref(dataFileName)
+        outputFilePath = destinationFolderName + outputFileName
 
         ##TODO: try catch!
         dataFile = open(dataFolder + dataFileName)
-        lines = [line.decode('utf-8').strip() for line in dataFile.readlines()]    # we'll use lines more than once, so let's get them on memory
-        title = str(index) + '. ' + getTitle(lines)
-        couplets = coupletCreator.createCouplets(lines)
-
-        htmlForFile = htmlGenerator.getHtmlForCouplets(couplets)
-
-        html = template
-        html = html.replace('#{title}', title.encode('utf-8'))
-        html = html.replace('#{content}', htmlForFile.encode('utf-8'))
-        html = html.replace('#{titleVar}', title.encode('utf-8'))
-        html = html.replace('#{urlVar}', outputFileName)
-        html = html.replace('#{nextPageUrl}', nextPageHref)
-
-        outputFilePath = destinationFolderName + outputFileName
         outputFile = open(outputFilePath, 'w')
-        outputFile.write(html)
+        try:
+            lines = [line.decode('utf-8').strip() for line in dataFile.readlines()]    # we'll use lines more than once, so let's get them on memory
+            title = str(index) + '. ' + getTitle(lines)
+            couplets = coupletCreator.createCouplets(lines)
 
+            htmlForFile = htmlGenerator.getHtmlForCouplets(couplets)
+
+            html = template
+            html = html.replace('#{title}', title.encode('utf-8'))
+            html = html.replace('#{content}', htmlForFile.encode('utf-8'))
+            html = html.replace('#{titleVar}', title.encode('utf-8'))
+            html = html.replace('#{urlVar}', outputFileName)
+            html = html.replace('#{nextPageUrl}', nextPageHref)
+
+            outputFile.write(html)
+
+            indexContent += '<li><a href="' + outputFileName + '">' + title + '</a></li>\n'
+        finally:
+            dataFile.close()
+            outputFile.close()
+
+    indexTemplateFile = open('templates/indexTemplate.html')
+    indexOutputFile = open(destinationFolderName + 'index.html', 'w')
+
+    try:
+        indexTemplate = indexTemplateFile.read()
+        html = indexTemplate.replace('#{index}', indexContent.encode('utf-8'))
+        indexOutputFile.write(html)
+    finally:
+        indexOutputFile.close()
+        indexTemplateFile.close()
 
 if __name__ == "__main__":
     sys.exit(main())
